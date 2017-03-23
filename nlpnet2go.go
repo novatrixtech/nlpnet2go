1b21b21d4010ec67730af81e5310fef2e14b1073
Edward Martins Jr <emartins@dropadev.local>
2017-03-23 18:06:01 -0300

    Setup .INI

--- nlpnet2go.go
+++ nlpnet2go.go
@@ -8,76 +8,96 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+
+	"github.com/go-ini/ini"
 )
 
 func main() {
 
 	args := os.Args[1:]
+	text2analize := ""
+	attribts := new(attributes)
+	setupfile := "setup.ini"
 
-	//TODO: Carregar setup através do .INI
-	attribts := attributes{}
-	attribts.cmdName = "python"
-	attribts.nlpnet2gopy = "nlpnet2go.py"
-	attribts.method = "pos"
-	attribts.port = ":5000"
+	//Carregar setup através do .ini
+	cfg, err := ini.Load(setupfile)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error:", err)
+		os.Exit(1)
+	}
+
+	err = cfg.Section("attributes").MapTo(attribts)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error:", err)
+		os.Exit(1)
+	}
 
 	/*Caso existam argumentos de entrada,
 	se comporta apenas como aplicação de console,
 	caso contrário se comporta como serviço de api web*/
 	if len(args) != 0 {
 		for i := 0; i < len(args); i++ {
-			attribts.text2analize += args[i] + " "
+			text2analize += args[i] + " "
 		}
-		log.Println(execNlpnetCommand(attribts))
+		log.Println(execNlpnetCommand(attribts, text2analize))
 
 	} else {
 		http.HandleFunc("/parser", func(w http.ResponseWriter, r *http.Request) {
 
 			/* Valida as entradas e carrega a estrutura */
 			if method := r.FormValue("method"); method != "" {
-				attribts.method = method
+				attribts.Method = method
+
+				/* Atualiza o arquivo de configuração em relação ao metodo de tagger
+				a fim de ser utilizado pelo script python */
+				cfg.Section("attributes").Key("method").SetValue(attribts.Method)
+				err = cfg.SaveTo(setupfile)
+				if err != nil {
+					fmt.Fprintln(os.Stderr, "Error:", err)
+					os.Exit(1)
+				}
 			}
 			if txt := r.FormValue("txt"); txt != "" {
-				attribts.text2analize = txt
+				text2analize = txt
 			}
 
 			/* Retorna a mensagem analisada */
-			io.WriteString(w, execNlpnetCommand(attribts))
+			io.WriteString(w, execNlpnetCommand(attribts, text2analize))
 
 		})
 
-		http.ListenAndServe(attribts.port, nil)
+		http.ListenAndServe(":"+attribts.Port, nil)
 	}
 
 }
 
-func execNlpnetCommand(attribts attributes) string {
+func execNlpnetCommand(attribts *attributes, text2analize string) string {
 
-	cmdArgs := []string{attribts.nlpnet2gopy, attribts.text2analize}
-	cmd := exec.Command(attribts.cmdName, cmdArgs...)
+	cmdArgs := []string{attribts.Nlpnet2gopy, text2analize}
+	cmd := exec.Command(attribts.CmdName, cmdArgs...)
 
-	/* Opção A código minificado */
-	// cmd.Stdout = os.Stdout
-	// cmd.Stderr = os.Stderr
-	// log.Println(cmd.Run())
-
-	/* Opção B código tratado */
 	cmdReader, err := cmd.Output()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error:", err)
 		os.Exit(1)
 	}
-	//TODO: Converter JSON
-	replacer := strings.NewReplacer("(u'", "{\"", "', u'", "\": \"", "')", "\"}")
-	//, "',", "\":", "(", "{", ")", "}", "'", "\"", "\",", "\":")
 
-	return replacer.Replace(string(cmdReader))
+	/* Conversão para JSON */
+	var result string
+	if attribts.Method == "pos" {
+		replacer := strings.NewReplacer("(u'", "{\"", "', u'", "\": \"", "')", "\"}")
+		result = replacer.Replace(string(cmdReader))
+	} else {
+		result = string(cmdReader)
+	}
+	return result
+
 }
 
 type attributes struct {
-	method       string
-	port         string
-	text2analize string
-	cmdName      string
-	nlpnet2gopy  string
+	Method      string `ini:"method"`
+	Port        string `ini:"port"`
+	CmdName     string `ini:"cmdName"`
+	Nlpnet2gopy string `ini:"nlpnet2gopy"`
+	Setdatadir  string `ini:"setdatadir"`
 }
--- nlpnet2go.py
+++ nlpnet2go.py
@@ -1,10 +1,25 @@
 """ Este script eh um script chamador da biblioteca de NLP.
 https://github.com/erickrf/nlpnet."""
 import sys
+try:
+    from configparser import ConfigParser
+except ImportError:
+    from ConfigParser import ConfigParser  # ver. < 3.0
 import nlpnet
 
 
-nlpnet.set_data_dir('/Users/emartins/nlpnet-data/')
-TAGGER = nlpnet.POSTagger()
-print TAGGER.tag(" ".join(sys.argv[1:]))
+CONFIG = ConfigParser()
+CONFIG.read('setup.ini')
+nlpnet.set_data_dir(CONFIG.get('attributes', 'setdatadir'))
+METHOD = CONFIG.get('attributes', 'method')
+if METHOD == "pos":
+    TAGGER = nlpnet.POSTagger()
+    print TAGGER.tag(" ".join(sys.argv[1:]))
+elif METHOD == "srl":
+    TAGGER = nlpnet.SRLTagger()
+    SENT = TAGGER.tag(" ".join(sys.argv[1:]))[0]
+    print SENT.arg_structures
+else:
+    print TAGGER.tag("Invalid Tagger method operator. Only 'pos' OR 'srl' allowed.")
+
 
